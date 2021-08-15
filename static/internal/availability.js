3f073e8814fcc1b40b6df29883ae5049be42b77c
GaboGomezT <gomezgabriel09@gmail.com>
2021-08-15 12:23:10 -0500

    improved schedule input

---
+++ static/internal/availability.js
@@ -0,0 +1,81 @@
+monday_adder.addEventListener("click", function() {
+    var container = document.getElementById("monday_container");
+    var input = document.createElement("input");
+    input.type = "time";
+    input.min = "08:00"
+    input.max = "24:00"
+    input.name = "monday";
+    container.appendChild(input);
+    // Append a line break 
+    container.appendChild(document.createElement("br"));
+});
+
+tuesday_adder.addEventListener("click", function() {
+    var container = document.getElementById("tuesday_container");
+    var input = document.createElement("input");
+    input.type = "time";
+    input.min = "08:00"
+    input.max = "24:00"
+    input.name = "tuesday";
+    container.appendChild(input);
+    // Append a line break 
+    container.appendChild(document.createElement("br"));
+});
+
+wednesday_adder.addEventListener("click", function() {
+    var container = document.getElementById("wednesday_container");
+    var input = document.createElement("input");
+    input.type = "time";
+    input.min = "08:00"
+    input.max = "24:00"
+    input.name = "wednesday";
+    container.appendChild(input);
+    // Append a line break 
+    container.appendChild(document.createElement("br"));
+});
+
+thursday_adder.addEventListener("click", function() {
+    var container = document.getElementById("thursday_container");
+    var input = document.createElement("input");
+    input.type = "time";
+    input.min = "08:00"
+    input.max = "24:00"
+    input.name = "thursday";
+    container.appendChild(input);
+    // Append a line break 
+    container.appendChild(document.createElement("br"));
+});
+
+friday_adder.addEventListener("click", function() {
+    var container = document.getElementById("friday_container");
+    var input = document.createElement("input");
+    input.type = "time";
+    input.min = "08:00"
+    input.max = "24:00"
+    input.name = "friday";
+    container.appendChild(input);
+    // Append a line break 
+    container.appendChild(document.createElement("br"));
+});
+saturday_adder.addEventListener("click", function() {
+    var container = document.getElementById("saturday_container");
+    var input = document.createElement("input");
+    input.type = "time";
+    input.min = "08:00"
+    input.max = "24:00"
+    input.name = "saturday";
+    container.appendChild(input);
+    // Append a line break 
+    container.appendChild(document.createElement("br"));
+});
+sunday_adder.addEventListener("click", function() {
+    var container = document.getElementById("sunday_container");
+    var input = document.createElement("input");
+    input.type = "time";
+    input.min = "08:00"
+    input.max = "24:00"
+    input.name = "sunday";
+    container.appendChild(input);
+    // Append a line break 
+    container.appendChild(document.createElement("br"));
+});
