22f8a5327eb6ea8bb74f3d6bf05df28666b48ee2
GaboGomezT <gomezgabriel09@gmail.com>
2021-08-26 08:49:36 -0500

    changed db table names

--- DDL/STARTUP.sql
+++ DDL/STARTUP.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS therapists (
+CREATE TABLE IF NOT EXISTS therapist (
 	id serial PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
 	middle_name VARCHAR(50),
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS therapists (
 );
 
 
-CREATE TABLE IF NOT EXISTS patients (
+CREATE TABLE IF NOT EXISTS patient (
 	id serial PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
 	middle_name VARCHAR(50),
@@ -19,27 +19,27 @@ CREATE TABLE IF NOT EXISTS patients (
     schedule VARCHAR(100) NOT NULL,
 	email VARCHAR(50) NOT NULL,
 	password VARCHAR(50) NOT NULL,
-    therapist_id INTEGER NOT NULL REFERENCES therapists (id),
+    therapist_id INTEGER NOT NULL REFERENCES therapist (id),
 	created_date TIMESTAMPTZ NOT NULL
 );
 
 
-CREATE TABLE IF NOT EXISTS events (
+CREATE TABLE IF NOT EXISTS event (
 	id serial PRIMARY KEY,
 	description VARCHAR(50),
 	start_date TIMESTAMPTZ NOT NULL,
 	end_date TIMESTAMPTZ NOT NULL,
 	frequency INTEGER NOT NULL,
     price_per_session INTEGER NOT NULL,
-    therapist_id INTEGER NOT NULL REFERENCES therapists (id),
-    patient_id INTEGER NOT NULL REFERENCES patients (id),
+    therapist_id INTEGER NOT NULL REFERENCES therapist (id),
+    patient_id INTEGER NOT NULL REFERENCES patient (id),
     is_active BOOLEAN NOT NULL DEFAULT TRUE,
     created_date TIMESTAMPTZ NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS event_history (
 	id serial PRIMARY KEY,
-	event_id INTEGER NOT NULL REFERENCES events (id),
+	event_id INTEGER NOT NULL REFERENCES event (id),
 	description VARCHAR(50),
 	date TIMESTAMPTZ NOT NULL,
     status VARCHAR(50) NOT NULL,
