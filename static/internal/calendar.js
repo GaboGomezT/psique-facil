9f84763f67888a7f4686a91d59c2f02129ea0b5b
GaboGomezT <gomezgabriel09@gmail.com>
2021-09-11 18:11:33 -0500

    :feature: Dynamic timezone selector

--- static/internal/calendar.js
+++ static/internal/calendar.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
+    var initialTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    var timeZoneSelectorEl = document.getElementById('time-zone-selector');
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
+        nowIndicator: true,
+        timeZone: initialTimeZone,
         locale: 'es',
         height: 650,
         headerToolbar: {
@@ -31,26 +35,51 @@ document.addEventListener('DOMContentLoaded', function () {
         events: [
             {
                 title: 'event1',
-                start: '2021-08-29T12:30:00',
+                start: '2021-09-11T12:30:00',
                 allDay: false
             },
             {
                 title: 'event1.5',
-                start: '2021-08-29T13:30:00',
+                start: '2021-09-11T13:30:00',
                 allDay: false
             },
             {
                 title: 'event2',
-                start: '2021-08-30T12:30:00',
-                end: '2021-08-30T13:30:00',
+                start: '2021-09-12T12:30:00',
+                end: '2021-09-12T13:30:00',
                 allDay: false
             },
             {
                 title: 'event3',
-                start: '2021-09-01T12:30:00',
+                start: '2021-09-14T12:30:00',
                 allDay: false // will make the time show
             }
         ]
     });
     calendar.render();
+
+    var opts = {
+        method: 'GET',
+        headers: {}
+    };
+    fetch('/timezones', opts).then(function (response) {
+        return response.json();
+    })
+        .then(function (body) {
+            body.timezones.forEach(function (timeZone) {
+                var optionEl;
+
+                optionEl = document.createElement('option');
+                optionEl.value = timeZone;
+                optionEl.innerText = timeZone;
+                timeZoneSelectorEl.appendChild(optionEl);
+            });
+
+            timeZoneSelectorEl.value = initialTimeZone;
+        });
+
+    // when the timezone selector changes, dynamically change the calendar option
+    timeZoneSelectorEl.addEventListener('change', function () {
+        calendar.setOption('timeZone', this.value);
+    });
 });
